3d60fe9fdb709606953e583ad7790153890a942d
Mahmoud SALMI <mahmoud.salmi@msweb.net>
2022-12-05 09:27:07 +0100

    Day 03 - Part1

---
+++ src/bin/day03.rs
@@ -0,0 +1,55 @@
+use std::{io::Result, collections::HashSet};
+
+const YEAR: &'static str = "2022";
+const DAY: &'static str = "03";
+
+fn priority(c: char) -> u64 {
+    match c {
+        min if min >= 'a' && min <= 'z' => 1_u64 + min as u64 - 'a' as u64,
+        max if max >= 'A' && max <= 'Z' => 27_u64 + max as u64 - 'A' as u64,
+        _ => unreachable!("Not alphabetic")
+    }
+}
+
+fn to_hashes(s: &str) -> HashSet<u64> {
+    s.chars()
+        .map(|c| priority(c))
+        .fold(HashSet::<u64>::new(), |mut res, priority| {
+            res.insert(priority);
+            res
+        })
+}
+
+fn part1(data: &str) -> u64 {
+    data.lines()
+        .map(|line| line.split_at((line.len() / 2).into()))
+        .map(|(left, right)| (to_hashes(left), to_hashes(right)))
+        .map(|(l, r)| l.iter()
+            .filter(|priority| r.contains(priority))
+            .map(|&p| p)
+            .sum::<u64>()
+        )
+        .sum::<u64>()
+}
+
+fn part2(_data: &str) -> u64 {
+    0
+}
+
+fn main() -> Result<()> {
+    println!();
+    println!("---( AOC{} - Day {} )-----------------------[Rust]----", YEAR, DAY);
+
+    let test_data = include_str!("day03.test");
+    println!("Test :: Part 1 ====>     {}", part1(&test_data));
+    println!("Test :: Part 2 ====>     {}", part2(&test_data));
+    println!("--------------------------------------------------------");
+    
+
+    let input_data = include_str!("day03.in");
+    println!("Input:: Part 1 ====>     {}", part1(&input_data));
+    println!("Input:: Part 2 ====>     {}", part2(&input_data));
+    println!("--------------------------------------------------------");
+
+    Ok(())
+}
